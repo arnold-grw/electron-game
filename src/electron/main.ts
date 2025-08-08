3f7c7186b501d301fcf0894655946e857f7af7b3
Arnold Grunwald <arnold.grunwald@stud.hs-hannover.de>
2025-08-09 00:46:47 +0200

    first setup

---
+++ src/electron/main.ts
@@ -0,0 +1,31 @@
+import { app, BrowserWindow } from 'electron';
+import { fileURLToPath } from 'url';
+import * as path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createWindow() {
+  const win = new BrowserWindow({
+    fullscreen: true, //fullscreen mode
+    webPreferences: {
+      //preload: path.join(__dirname, 'preload.js'), // optional
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  if (process.env.VITE_DEV_SERVER_URL) {
+    // dev mode loads the Vite dev server
+    win.loadURL(process.env.VITE_DEV_SERVER_URL);
+  } else {
+    // production mode loads the built renderer files (index.html)
+    win.loadFile(path.join(__dirname, '../renderer/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
